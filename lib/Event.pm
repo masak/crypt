cd559eedcca90002ca3e1985e42abde887621646
Carl Masak <cmasak@gmail.com>
2012-11-19 15:16:07 +0100

    [Event] removed
    It is found in Adventure::Engine. No need to keep our own
    (identical) version of it.

--- lib/Event.pm
+++
@@ -1,9 +0,0 @@
-role Event {
-    method Str {
-        sub name($attr) { $attr.name.substr(2) }
-        sub value($attr) { $attr.get_value(self) }
-        sub attrpair($attr) { ":{name $attr}<{value $attr}>" }
-
-        sprintf '%s[%s]', self.^name, ~map &attrpair, self.^attributes;
-    }
-}
